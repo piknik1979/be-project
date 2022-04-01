a34fd50510090873c1508b31010f3fb9e63001ea
piknik1979 <tpikniczka@gmail.com>
2022-04-01 11:05:09 +0100

    selectArticles module moved back to the module file :)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -179,16 +179,3 @@ describe("GET /api/users", () => {
       });
   });
 });
-
-exports.selectArticles = () => {
-  let queryStr = `SELECT articles.*, 
-    COUNT(comments.comment_id) AS comment_count FROM articles
-    LEFT JOIN comments
-    ON comments.article_id = articles.article_id
-    GROUP BY articles.article_id
-    ORDER BY created_at DESC;`;
-
-  return db.query(queryStr).then((result) => {
-    return result.rows;
-  });
-};
